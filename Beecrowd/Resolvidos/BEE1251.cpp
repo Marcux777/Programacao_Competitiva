1e5a27956c4bd8fccb04998b19b9ed36f17241f1
Marcus Vinicius <marcusviny63@gmail.com>
2023-04-23 14:55:40 -0300

    .

--- Beecrowd/Resolvidos/BEE1251.cpp
+++ Beecrowd/Resolvidos/BEE1251.cpp
@@ -7,7 +7,10 @@ using namespace std;
 
 bool compareBySecond(const pair<char, int> &a, const pair<char, int> &b)
 {
-    return a.second < b.second;
+    if(a.second != b.second){
+        return a.second < b.second;
+    }
+    return (int)a.first > (int)b.first; 
 }
 
 vector<pair<char, int>> sortMapByValue(const map<char, int> &myMap)
@@ -23,10 +26,11 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    //freopen("saida.txt", "w", stdout);
     string str;
     bool aux = false;
 
-    while (cin >> str)
+    while (getline(cin, str))
     {
         if (aux)
         {
@@ -36,6 +40,7 @@ int main()
         map<char, int> mapa;
         for (int i = 0; i < str.size(); i++)
         {
+            
             auto it = mapa.find(str[i]);
             if (it != mapa.end())
             {
