adbdac9bb83193115513b3bf1a9303fe9cbfd2e9
Marcux777 <marcuviny63@gmail.com>
2024-06-01 10:41:51 -0300

    chore: Add settings.json and tasks.json for AtCoder Beginner Contest 356

---
+++ AtCoder Beginner Contest 356/E.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0)
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define f first
+#define s second
+#define Unique(v) \
+    sort(all(v)); \
+    v.erase(unique(all(v)), v.end());
+#define sz(v) ((int)v.size())
+#define sor(x) sort(all(x))
+#define ft front()
+#define bk back()
+#define endl "\n"
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef long long ll;
+typedef double db;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<pii> vii;
+typedef vector<piii> viii;
+const int MAXN = 2e5 + 5;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const int mod = 1e9 + 7;
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << _VA_ARGS_ << "):", dbg_out(_VA_ARGS_), cerr << endl
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi A(n);
+    for(int i = 0; i < n; i++) cin >> A[i];
+    map<int, int> freq;
+    for (int a : A) freq[a]++;
+    double sum = 0;
+    for (auto it = freq.begin(); it != freq.end(); ++it) {
+        for (auto jt = next(it); jt != freq.end(); ++jt) {
+            sum += (int)((double)((1.0 * jt->first) / (1.0*it->first)) * 1.0 *it->second * jt->second);
+            sum += (int)((double)((1.0 * it->first) / (1.0*jt->first))* 1.0* it->second * jt->second);
+        }
+    }
+    cout << sum << endl;
+}
+
+int32_t main()
+{
+    IOS;
+    int tt;
+    tt = 1;
+    while (tt--)
+        solve();
+    return 0;
+}
