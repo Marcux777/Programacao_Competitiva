bff1fef16a678efee5f68b5aba3087edd7d94f47
Marcus Vinicius <77116012+Marcux777@users.noreply.github.com>
2024-09-20 12:59:34 -0300

    USACO - Simulation problems

---
+++ USACO/855 - Mixing Milk.cpp
@@ -0,0 +1,94 @@
+#if defined(LOCAL) or not defined(LUOGU)
+#pragma GCC optimize(3)
+#pragma GCC optimize("Ofast,unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0)
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define f first
+#define s second
+#define Unique(v)                     \
+    sort(all(v));                     \
+    v.erase(unique(all(v)), v.end()); \
+    v.shrink_to_fit()
+#define sz(v) ((int)v.size())
+#define sor(x) sort(all(x))
+#define ft front()
+#define bk back()
+#define endl "\n"
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define LB(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define UB(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef long long ll;
+typedef double db;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<pii> vii;
+typedef vector<piii> viii;
+typedef tuple<int, int, int> tiii;
+const int MAXN = 2e5 + 5;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const int mod = 1e9 + 7;
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << _VA_ARGS_ << "):", dbg_out(_VA_ARGS_), cerr << endl
+
+void solve(ifstream& cin, ofstream& cout)
+{
+    int c[3];
+    int milk[3];
+    for(int i = 0; i < 3; i++){
+        cin >> c[i] >> milk[i];
+    }
+    
+    for (int i = 0; i < 100; ++i) {
+        int from = i % 3;
+        int to = (i + 1) % 3;
+
+        int p = min(milk[from], c[to] - milk[to]);
+        milk[from] -= p;
+        milk[to] += p;
+    }
+
+    for (int i = 0; i < 3; ++i) {
+        cout << milk[i] << endl;
+    }
+
+}
+
+int32_t main()
+{
+    ifstream in("mixmilk.in");
+    ofstream out("mixmilk.out");
+    IOS;
+    int tt;
+    tt = 1;
+    while (tt--)
+        solve(in, out);
+    in.close();
+    out.close();
+    return 0;
+}
