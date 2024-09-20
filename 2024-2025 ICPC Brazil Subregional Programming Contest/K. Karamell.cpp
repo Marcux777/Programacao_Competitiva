e4cb9c6cfddfbeb9bb0ff12e907be40a0b1cd173
Marcux777 <marcuviny63@gmail.com>
2024-09-20 15:04:31 -0300

    Subregional 2024-2025

---
+++ 2024-2025 ICPC Brazil Subregional Programming Contest/K. Karamell.cpp
@@ -0,0 +1,114 @@
+#if defined(LOCAL) or not defined(LUOGU)
+#pragma GCC optimize(3)
+#pragma GCC optimize("Ofast,unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0)
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define f first
+#define s second
+#define Unique(v)                     \
+    sort(all(v));                     \
+    v.erase(unique(all(v)), v.end()); \
+    v.shrink_to_fit()
+#define sz(v) ((int)v.size())
+#define sor(x) sort(all(x))
+#define ft front()
+#define bk back()
+#define endl "\n"
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define LB(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define UB(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef long long ll;
+typedef double db;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<pii> vii;
+typedef vector<piii> viii;
+typedef tuple<int, int, int> tiii;
+const int MAXN = 2e5 + 5;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const int mod = 1e9 + 7;
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << _VA_ARGS_ << "):", dbg_out(_VA_ARGS_), cerr << endl
+
+bool f(vi& n){
+    int s = accumulate(all(n), 0);
+    if(s%2 != 0) return false;
+    int t = (s >> 1);
+    vector<bool> dp(t + 1, false);
+    dp[0] = true;
+    for(auto i : n)
+        for(int j = t; j >= i; j--)
+            dp[j] = dp[j] || dp[j - i];
+
+    return dp[t];
+
+}
+
+void solve()
+{
+    int n; cin >> n;
+    vi a(n);
+    for(auto &i : a) cin >> i;
+
+    if(f(a)){
+        sort(a.rbegin(), a.rend());
+        int alice = 0, bob = 0;
+        vi al, bo;
+        for(int i = 0; i < n; i++){
+            if(alice <= bob){
+                alice += a[i];
+                al.pb(a[i]);
+            }else{
+                bob += a[i];
+                bo.pb(a[i]);
+            }
+        }
+        for(int i = 0, j = 0, k = 0; i < n; i++){
+            if(i%2 && j < sz(al)){
+                cout << al[j++] << ' ';
+            }else if(k < sz(bo)){
+                cout << bo[k++] << ' ';
+            }
+        }
+        cout << endl;
+        
+    }
+    else cout << -1 << endl;
+
+}
+
+int32_t main()
+{
+    IOS;
+    int tt;
+    tt = 1;
+    while (tt--)
+        solve();
+    return 0;
+}
