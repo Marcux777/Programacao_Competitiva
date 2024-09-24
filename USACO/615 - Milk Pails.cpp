36b49e0df24a92fcaf3cff05c64ab893cca97edd
Marcux777 <marcuviny63@gmail.com>
2024-09-23 23:15:18 -0300

    Add solutions for USACO problems 615 - Milk Pails and 639 - Diamond Collector

---
+++ USACO/615 - Milk Pails.cpp
@@ -0,0 +1,97 @@
+#if defined(LOCAL) or not defined(LUOGU)
+#pragma GCC optimize(3)
+#pragma GCC optimize("Ofast,unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0)
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define f first
+#define s second
+#define Unique(v)                     \
+    sort(all(v));                     \
+    v.erase(unique(all(v)), v.end()); \
+    v.shrink_to_fit()
+#define sz(v) ((int)v.size())
+#define sor(x) sort(all(x))
+#define ft front()
+#define bk back()
+#define endl "\n"
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define LB(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define UB(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef long long ll;
+typedef double db;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<pii> vii;
+typedef vector<piii> viii;
+typedef tuple<int, int, int> tiii;
+const int MAXN = 2e5 + 5;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const int mod = 1e9 + 7;
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << _VA_ARGS_ << "):", dbg_out(_VA_ARGS_), cerr << endl
+
+int x, y, m;
+
+vi dp(1000000, -1);
+
+int f(int sum){
+    if(dp[sum] != -1) return dp[sum];
+    if(sum == m)
+        return sum;
+    if(sum > m)
+        return -1;
+
+    int a = max(f(sum + x), sum);
+    int b = max(f(sum + y), sum);
+
+    return dp[sum] = max(a, b);
+}
+//
+void solve(ifstream& cin, ofstream& cout)
+{
+    
+    cin >> x >> y >> m;
+    
+    cout << f(0) << endl;
+
+}
+
+int32_t main()
+{
+    IOS;
+    ifstream in("pails.in");
+    ofstream out("pails.out");
+    int tt;
+    tt = 1;
+    while (tt--)
+        solve(in, out);
+    in.close();
+    out.close();
+    return 0;
+}
---
+++ USACO/639 - Diamond Collector.cpp
@@ -0,0 +1,96 @@
+#if defined(LOCAL) or not defined(LUOGU)
+#pragma GCC optimize(3)
+#pragma GCC optimize("Ofast,unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0)
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define f first
+#define s second
+#define Unique(v)                     \
+    sort(all(v));                     \
+    v.erase(unique(all(v)), v.end()); \
+    v.shrink_to_fit()
+#define sz(v) ((int)v.size())
+#define sor(x) sort(all(x))
+#define ft front()
+#define bk back()
+#define endl "\n"
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define LB(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define UB(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef long long ll;
+typedef double db;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<pii> vii;
+typedef vector<piii> viii;
+typedef tuple<int, int, int> tiii;
+const int MAXN = 2e5 + 5;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const int mod = 1e9 + 7;
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << _VA_ARGS_ << "):", dbg_out(_VA_ARGS_), cerr << endl
+
+int n, k;
+vi v;
+
+void solve(ifstream &cin, ofstream& cout)
+{
+    cin >> n >> k;
+    v = vi(n);
+
+    for(auto &i : v)cin>>i;
+
+    sor(v);
+
+    int ans = -1;
+    for(int i = 0; i < n; i++){
+        int curr = 1;
+        for(int j = i+1; j < n; j++)
+            if(abs(v[i] - v[j]) <= k)
+                curr++;
+        ans = max(ans, curr);
+
+    }
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    IOS;
+    ifstream in("diamond.in");
+    ofstream out("diamond.out");
+    int tt;
+    tt = 1;
+    while (tt--)
+        solve(in, out);
+
+    in.close();
+    out.close();
+    return 0;
+}
