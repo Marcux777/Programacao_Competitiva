75a8b32be9b4172918ae5c882e849b3ecf215b2f
Marcux777 <marcuviny63@gmail.com>
2024-10-11 12:13:47 -0300

    Add code for CSES Forest Queries problem

---
+++ Beecrowd/Estrutura de Dados/BEE 1425.cpp
@@ -0,0 +1,112 @@
+#if defined(LOCAL) or not defined(LUOGU)
+#pragma GCC optimize(3)
+#pragma GCC optimize("Ofast,unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0)
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define f first
+#define s second
+#define Unique(v)                     \
+    sort(all(v));                     \
+    v.erase(unique(all(v)), v.end()); \
+    v.shrink_to_fit()
+#define sz(v) ((int)v.size())
+#define sor(x) sort(all(x))
+#define ft front()
+#define bk back()
+#define endl "\n"
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define LB(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define UB(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef long long ll;
+typedef double db;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<pii> vii;
+typedef vector<piii> viii;
+typedef tuple<int, int, int> tiii;
+const int MAXN = 2e5 + 5;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const int mod = 1e9 + 7;
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << _VA_ARGS_ << "):", dbg_out(_VA_ARGS_), cerr << endl
+
+void solve()
+{
+    int n, m;
+    while (cin >> n >> m && n && m) {
+
+        if (m > 34) {
+            cout << "Let me try!" << endl;
+            continue;
+        }
+
+        queue<pair<int, int>> q;
+        q.push({1, 2});
+        bool found = false;
+
+        while (!q.empty()) {
+            auto current = q.front();
+            q.pop();
+            int pos = current.first;
+            int i = current.second;
+
+            if (pos == m) {
+                cout << "Let me try!" << endl;
+                found = true;
+                break;
+            }
+
+            int step = 2 * i - 1;
+
+            int n_pos = pos + step;
+            if (n_pos <= n) {
+                q.push({n_pos, i + 1});
+            }
+
+            int n_pos2 = pos - step;
+            if (n_pos2 > 0) {
+                q.push({n_pos2, i + 1});
+            }
+        }
+
+        if (!found) {
+            cout << "Don't make fun of me!" << endl;
+        }
+    }
+}
+
+int32_t main()
+{
+    IOS;
+    int tt;
+    tt = 1;
+    while (tt--)
+        solve();
+    return 0;
+}
