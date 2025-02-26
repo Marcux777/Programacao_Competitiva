0c60ade08eada473638c5f3bf2ccc8eca29e189b
Marcus Vinicius <77116012+Marcux777@users.noreply.github.com>
2025-02-26 14:22:51 +0000

    Adicionar soluções para os problemas A, G, H e C do Codeforces Round 898 e 1006, incluindo otimizações e estrutura de dados apropriadas.

---
+++ Codeforces Round 1006 (Div. 3)/D.cpp
@@ -0,0 +1,118 @@
+/*
+~~ Alguma parte/frase foda de um livro/mangá para dar sorte ~~
+
+Uma vez eu gritei, gradualmente, perdi minha voz.
+Uma vez eu chorei, gradualmente, perdi minhas lágrimas.
+Uma vez eu sofri, gradualmente, me tornei capaz de suportar tudo.
+Uma vez me alegrei, gradualmente, me tornei indiferente ao mundo.
+E agora, tudo o que me resta é um rosto sem expressão,
+meu olhar é tão firme quanto um monólito,
+apenas a perseverança permanece no meu coração.
+Este sou eu, um personagem insignificante,
+Fang Yuan — A Perseverança.
+
+*/
+#if defined(LOCAL) or not defined(LUOGU)
+#pragma GCC optimize(3)
+#pragma GCC optimize("Ofast,unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define int long long
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0)
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define f first
+#define s second
+#define Unique(v)                     \
+    sort(all(v));                     \
+    v.erase(unique(all(v)), v.end()); \
+    v.shrink_to_fit()
+#define sz(v) ((int)v.size())
+#define sor(x) sort(all(x))
+#define ft front()
+#define bk back()
+#define endl "\n"
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define LB(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define UB(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef long long ll;
+typedef double db;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<pii> vii;
+typedef vector<piii> viii;
+typedef tuple<int, int, int> tiii;
+const int MAXN = 2e5 + 5;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const int mod = 1e9 + 7;
+const int LOGN = 21;
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << _VA_ARGS_ << "):", dbg_out(_VA_ARGS_), cerr << endl
+
+int cmp(int x, int y){
+    if(x > y) return 1;
+    if(x < y) return -1;
+    return 0;
+}
+
+void solve()
+{
+    int n; cin >> n;
+    vi a(n+1);
+    rep(i, 0, n) cin >> a[i+1];
+
+    int b = LINF;
+    int l = 1, r = 1;
+    rep(i, 1, n){
+        int pref = 0;
+        rep(j, 1, n - i + 1){
+            pref += cmp(a[i + j], a[i]);
+            if(pref < b){
+                b = pref;
+                l = i;
+                r = i + j;
+            }
+        }
+    }
+    if(is_sorted(all(a))) cout << 1 << " " << 1 << endl;
+    else cout << l << " " << r << endl;
+
+}
+
+int32_t main()
+{
+    IOS;
+    int tt;
+    tt = 1;
+    cin >> tt;
+    while (tt--)
+        solve();
+    return 0;
+}
