af815c40c78c7efff27c3d24c9fda0532381b27b
Marcus Vinicius <77116012+Marcux777@users.noreply.github.com>
2025-04-10 23:42:23 +0000

    Adicionar soluções para os problemas do Codeforces Round 1016 (Div. 3), incluindo implementações para A, B, C, D, E, F e otimizações na função de resolução.

---
+++ Codeforces Round 1016 (Div. 3)/C.cpp
@@ -0,0 +1,120 @@
+/*
+~~ Alguma parte/frase foda de um livro/mangá para dar sorte ~~
+
+Uma vez eu gritei, gradualmente, perdi minha voz.
+Uma vez eu chorei, gradualmente, perdi minhas lágrimas.
+Uma vez eu sofri, gradualmente, me tornei capaz de suportar tudo.
+Uma vez me alegrei, gradualmente, me tornei indiferente ao mundo.
+E agora, tudo o que me resta é um rosto sem expressão,
+meu olhar é tão firme quanto um monólito,
+apenas a perseverança permanece no meu coração.
+Este sou eu, um personagem insignificante,
+Fang Yuan — A Perseverança.
+
+*/
+#if defined(LOCAL) or not defined(LUOGU)
+#pragma GCC optimize(3)
+#pragma GCC optimize("Ofast,unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T>
+ostream& operator<<(ostream &os, const vector<T> &v) {
+    os << "[";
+    for (size_t i = 0; i < v.size(); ++i) {
+        os << v[i] << (i + 1 == v.size() ? "" : ", ");
+    }
+    os << "]";
+    return os;
+}
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << _VA_ARGS_ << "):", dbg_out(_VA_ARGS_), cerr << endl
+
+#define int long long
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0)
+#define TXTIO                           \
+    freopen("entrada.in", "r", stdin);\
+    freopen("saida.out", "w", stdout)
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define f first
+#define s second
+#define Unique(v)                     \
+    sort(all(v));                     \
+    v.erase(unique(all(v)), v.end()); \
+    v.shrink_to_fit()
+#define sz(v) ((int)v.size())
+#define sor(x) sort(all(x))
+#define ft front()
+#define bk back()
+#define endl "\n"
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define LB(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define UB(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef long long ll;
+typedef double db;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<pii> vii;
+typedef vector<piii> viii;
+typedef tuple<int, int, int> tiii;
+const int MAXN = 2e5 + 5;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const int mod = 1e9 + 7;
+const int LOGN = 21;
+
+bool isprime(int x){
+    if(x == 1) return false;
+    if(x % 2 == 0) return x == 2;
+    rep(i, 3, sqrt(x) + 1){
+        if(x % i == 0) return false;
+    }
+    return true;
+}
+
+void solve()
+{
+    int n, k; cin >> n >> k;
+    if(k == 1){
+        cout << (isprime(n) ? "YES" : "NO") << endl;
+    }else if(n == 1 && k == 2) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+
+int32_t main()
+{
+    IOS;
+    int tt;
+    tt = 1;
+    cin >>tt;
+    while (tt --> 0)
+        solve();
+    return 0;
+}
--- I Maratona do Cerrado/C.cpp
+++ I Maratona do Cerrado/C.cpp
@@ -11,10 +11,13 @@ signed main() {
     IOS;
     string s;
     while(getline(cin, s)) {
+        string ans;
         int n = s.size();
         for(int i = 0; i < n; i++){
-            
+            if(s[i] == 'C' && i > 0 && isalpha(s[i-1]) && !isalpha(s[i+1])){
+                ans+="-se";
+            }else ans+=s[i];
         }
-
+        cout << ans << endl;
     }
 }
