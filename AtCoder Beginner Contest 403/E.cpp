26c88898856541a044aec98506e9a9bb921e82b9
Marcus Vinicius <77116012+Marcux777@users.noreply.github.com>
2025-05-10 16:29:48 +0000

    Add solutions for AtCoder Beginner Contest 403 and 404
    - Implemented problem E in Contest 403 using Trie data structure for prefix management.
    - Implemented problem F in Contest 403 with a placeholder for future logic.
    - Implemented problem A in Contest 404 to find the first missing lowercase letter.
    - Implemented problem B in Contest 404 to count minimum rotations needed to match two grids.
    - Implemented problem C in Contest 404 using a recursive approach to check conditions on subsets.
    - Implemented problem D in Contest 404 to validate a graph structure based on degree conditions.
    - Implemented problem E in Contest 404 to calculate the minimum cost based on given constraints.
    - Implemented problem F in Contest 404 with a placeholder for future logic.
    - Implemented problem G in Contest 404 with a placeholder for future logic.

---
+++ AtCoder Beginner Contest 403/E.cpp
@@ -0,0 +1,191 @@
+/*
+~~ Alguma parte/frase foda de um livro/mangá para dar sorte ~~
+
+Uma vez eu gritei, gradualmente, perdi minha voz.
+Uma vez eu chorei, gradualmente, perdi minhas lágrimas.
+Uma vez eu sofri, gradualmente, me tornei capaz de suportar tudo.
+Uma vez me alegrei, gradualmente, me tornei indiferente ao mundo.
+E agora, tudo o que me resta é um rosto sem expressão,
+meu olhar é tão firme quanto um monólito,
+apenas a perseverança permanece no meu coração.
+Este sou eu, um personagem insignificante,
+Fang Yuan — A Perseverança.
+
+*/
+#if defined(LOCAL) or not defined(LUOGU)
+#pragma GCC optimize(3)
+#pragma GCC optimize("Ofast,unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T>
+ostream& operator<<(ostream &os, const vector<T> &v) {
+    os << "[";
+    for (size_t i = 0; i < v.size(); ++i) {
+        os << v[i] << (i + 1 == v.size() ? "" : ", ");
+    }
+    os << "]";
+    return os;
+}
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << _VA_ARGS_ << "):", dbg_out(_VA_ARGS_), cerr << endl
+
+#define int long long
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0)
+#define TXTIO                           \
+    freopen("entrada.in", "r", stdin);\
+    freopen("saida.out", "w", stdout)
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define f first
+#define s second
+#define Unique(v)                     \
+    sort(all(v));                     \
+    v.erase(unique(all(v)), v.end()); \
+    v.shrink_to_fit()
+#define sz(v) ((int)v.size())
+#define sor(x) sort(all(x))
+#define ft front()
+#define bk back()
+#define endl "\n"
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define LB(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define UB(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef long long ll;
+typedef double db;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<pii> vii;
+typedef vector<piii> viii;
+typedef tuple<int, int, int> tiii;
+const int MAXN = 2e5 + 5;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const int mod = 1e9 + 7;
+const int LOGN = 21;
+
+struct Trie {
+	vector<vector<int>> to;
+	vector<int> end, pref;
+	int sigma; char norm;
+	Trie(int sigma_=26, char norm_='a') : sigma(sigma_), norm(norm_) {
+		to = {vector<int>(sigma)};
+		end = {0}, pref = {0};
+	}
+	void insert(string s) {
+		int x = 0;
+		for (auto c : s) {
+			int &nxt = to[x][c-norm];
+			if (!nxt) {
+				nxt = to.size();
+				to.push_back(vector<int>(sigma));
+				end.push_back(0), pref.push_back(0);
+			}
+			x = nxt, pref[x]++;
+		}
+		end[x]++, pref[0]++;
+	}
+	void erase(string s) {
+		int x = 0;
+		for (char c : s) {
+			int &nxt = to[x][c-norm];
+			x = nxt, pref[x]--;
+			if (!pref[x]) nxt = 0;
+		}
+		end[x]--, pref[0]--;
+	}
+	int find(string s) {
+		int x = 0;
+		for (auto c : s) {
+			x = to[x][c-norm];
+			if (!x) return -1;
+		}
+		return x;
+	}
+	int count_pref(string s) {
+		int id = find(s);
+		return id >= 0 ? pref[id] : 0;
+	}
+
+    bool has_prefix(const string& s){
+        int x = 0;
+        for(char c : s){
+            if(end[x]) return true;
+            x = to[x][c - norm];
+            if(!x) return false;
+        }
+        return end[x];
+    }
+};
+
+void solve()
+{
+    int q; cin >> q;
+    Trie t;
+    vector<string> s;
+    vector<bool> vis;
+    vi ativo;
+    int cnt = 0;
+    while(q--){
+        int op; cin >> op;
+        string st; cin >> st;
+        if(op == 1){
+            t.insert(st);
+            vi novo;
+            for(auto i : ativo){
+                if(!t.has_prefix(s[i])){
+                    novo.pb(i);
+                }else{
+                    cnt--;
+                    vis[i] = false;
+                }
+            }
+            ativo = move(novo);
+        }
+        else {
+            s.pb(st);
+            vis.pb(!t.has_prefix(st));
+            if(vis.back()){
+                ativo.pb(sz(s)-1);
+                cnt++;
+            }
+        }
+        cout << cnt << endl;
+    }
+}
+
+int32_t main()
+{
+    IOS;
+    int tt;
+    tt = 1;
+    while (tt --> 0)
+        solve();
+    return 0;
+}
---
+++ AtCoder Beginner Contest 404/C.cpp
@@ -0,0 +1,171 @@
+/*
+~~ Alguma parte/frase foda de um livro/mangá para dar sorte ~~
+
+Uma vez eu gritei, gradualmente, perdi minha voz.
+Uma vez eu chorei, gradualmente, perdi minhas lágrimas.
+Uma vez eu sofri, gradualmente, me tornei capaz de suportar tudo.
+Uma vez me alegrei, gradualmente, me tornei indiferente ao mundo.
+E agora, tudo o que me resta é um rosto sem expressão,
+meu olhar é tão firme quanto um monólito,
+apenas a perseverança permanece no meu coração.
+Este sou eu, um personagem insignificante,
+Fang Yuan — A Perseverança.
+
+*/
+#if defined(LOCAL) or not defined(LUOGU)
+#pragma GCC optimize(3)
+#pragma GCC optimize("Ofast,unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T>
+ostream& operator<<(ostream &os, const vector<T> &v) {
+    os << "[";
+    for (size_t i = 0; i < v.size(); ++i) {
+        os << v[i] << (i + 1 == v.size() ? "" : ", ");
+    }
+    os << "]";
+    return os;
+}
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << "(" << _VA_ARGS_ << "):", dbg_out(_VA_ARGS_), cerr << endl
+
+#define int long long
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0)
+#define TXTIO                           \
+    freopen("entrada.in", "r", stdin);\
+    freopen("saida.out", "w", stdout)
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define f first
+#define s second
+#define Unique(v)                     \
+    sort(all(v));                     \
+    v.erase(unique(all(v)), v.end()); \
+    v.shrink_to_fit()
+#define sz(v) ((int)v.size())
+#define sor(x) sort(all(x))
+#define ft front()
+#define bk back()
+#define endl "\n"
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define LB(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define UB(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvd> vvvd;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef long long ll;
+typedef double db;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<pii> vii;
+typedef vector<piii> viii;
+typedef tuple<int, int, int> tiii;
+const int MAXN = 2e5 + 5;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const int mod = 1e9 + 7;
+const int LOGN = 21;
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class DSU
+{
+    vector<int> parent;
+    vector<int> rank;
+
+public:
+    DSU(int n)
+    {
+        parent.assign(n, -1);
+        rank.assign(n, 1);
+    }
+
+    int find(int i)
+    {
+        if (parent[i] == -1)
+            return i;
+
+        return parent[i] = find(parent[i]);
+    }
+
+    void unite(int x, int y)
+    {
+        int s1 = find(x);
+        int s2 = find(y);
+
+        if (s1 != s2)
+        {
+            if (rank[s1] < rank[s2])
+            {
+                parent[s1] = s2;
+            }
+            else if (rank[s1] > rank[s2])
+            {
+                parent[s2] = s1;
+            }
+            else
+            {
+                parent[s2] = s1;
+                rank[s1] += 1;
+            }
+        }
+    }
+};
+
+void solve()
+{
+    int n, m; cin >> n >> m;
+    DSU dsu(n+1);
+    vi deg(n+1, 0);
+    bool ok = true;
+    rep(i, 0, m){
+        int a, b; cin >> a >> b;
+        deg[a]++;
+        deg[b]++;
+        if(dsu.find(a) != dsu.find(b)){
+            dsu.unite(a, b);
+        }
+    }
+    rep(i, 1, n+1) if(deg[i] != 2) ok = false;
+
+    int r = dsu.find(1);
+    rep(i, 2, n+1) if(dsu.find(i) != r) ok = false;
+
+    cout << (ok ? "Yes" : "No") << endl;
+}
+
+int32_t main()
+{
+    IOS;
+    int tt;
+    tt = 1;
+    while (tt --> 0)
+        solve();
+    return 0;
+}
